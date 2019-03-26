52f37a45282ec19111c57c12ca88e4b84277b703
Poorn Mehta <Poorn.Mehta@Colorado.EDU>
2019-03-26 00:07:05 -0600

    Temperature Sensor Final Version - All Features Working

--- Poorn_Files/I2C_Testing/TempSensor.c
+++ Poorn_Files/I2C_Testing/TempSensor.c
@@ -1,260 +1,271 @@
 #include "TempSensor.h"
 
-int fd;
-char *bus = "/dev/i2c-2";    // P9_19 and P9_20 
-int addr = 0x48;             
-char buf[2] = {0},pika=0x00;
-int temp;
-unsigned char MSB, LSB;
-float f,c,k; 
+int temp_file_des = -1;
 
-typedef enum
+uint8_t custom_temp_reg_write(uint8_t r_addr, uint16_t r_val)
 {
-	TEMP_CEL = 0,
-	TEMP_KEL = 1,
-	TEMP_FAH = 2
-}temp_format;
-
-int default_config_byte_one = 0x50;
-int default_config_byte_two = 0xA0;
-
-void write_pointer_reg(uint8_t value)
-{
-	if(write(fd, &value, 1) != 1)
-	{
-		perror("Write Pointer Register Error\n");
-	}
-}
-
-int write_tlow_reg(int reg, uint16_t value )
-{
-	write_pointer_reg(reg);
-	
-	uint8_t tmp = value >> 8;
-	value &= 0x00FF;
-	uint8_t buf[3] = {0x03, tmp, value};
-
-	if (write(fd, &buf, 3) != 3)
-	{
-		perror("T-low register write error");
-		return -1;
-	}
-
-	return 0;
-}
-
-
-int write_config_reg_on_off(uint8_t value )
-{
-	write_pointer_reg(0b00000001);
-	if((value == 0) || (value == 1))
-	{
-		default_config_byte_one |= value;
-		
-		if (write(fd, &default_config_byte_one, 1) != 1)
+		static uint8_t r_arr[3];
+		r_arr[0] = r_addr;
+		r_arr[1] = r_val >> 8;
+		r_arr[2] = r_val & 0x00FF;
+		if((r_addr < 1) || (r_addr > 3))
 		{
-			perror("Configuration register write error for first byte");
-			return -1;
+				perror("\nInvalid Register Address Supplied (Temp Sensor)\n");
+				return 1;
 		}
-		
-		if (write(fd, &default_config_byte_two, 1) != 1)
+		if(write_reg_ptr(&r_addr))
 		{
-			perror("Configuration register write error for second byte");
-			return -1;
+				perror("\nError in write_reg_ptr\n");
+				return 1;
 		}
-	}
-	return 0;
-}
-
-int write_config_reg_em(uint8_t value )
-{
-	write_pointer_reg(0b00000001);
-	if((value == 0) || (value == 1))
-	{
-		default_config_byte_two |= (value << 4);
-		
-		if (write(fd, &default_config_byte_one, 1) != 1)
+		if(write_reg(&r_arr))
 		{
-			perror("Configuration register write error for first byte");
-			return -1;
+				perror("\nError in write_reg_conf\n");
+				return 1;
 		}
-		
-		if (write(fd, &default_config_byte_two, 1) != 1)
+		return 0;
+}
+
+uint8_t custom_temp_reg_read(uint8_t r_addr, uint8_t *r_val)
+{
+		if(write_reg_ptr(&r_addr))
 		{
-			perror("Configuration register write error for second byte");
-			return -1;
+				perror("\nError in write_reg_ptr\n");
+				return 1;
 		}
-	}
-	return 0;
-}
-
-int write_config_reg_conv_rate(uint8_t value )
-{
-	write_pointer_reg(0b00000001);
-	if((value >= 0) || (value <= 3))
-	{
-		default_config_byte_two |= (value << 6);
-		if (write(fd, &default_config_byte_one, 1) != 1)
+		if(read_reg(r_val))
 		{
-			perror("Configuration register write error for first byte");
-			return -1;
+				perror("\nError in write_reg_conf\n");
+				return 1;
 		}
-		if (write(fd, &default_config_byte_two, 1) != 1)
+		return 0;
+}
+
+uint8_t custom_set_temp_thresholds(void)
+{
+		static uint8_t temp_reg_return[2];
+		// Setting THigh
+		if(custom_temp_reg_write(Temp_THigh_Reg, Temp_High_Threshold << 8))
 		{
-			perror("Configuration register write error for second byte");
-			return -1;
+				perror("\nFailed: custom_temp_reg_write\n");
+				return 1;
 		}
-	}
-	return 0;
+		if(custom_temp_reg_read(Temp_THigh_Reg, &temp_reg_return[0]))
+		{
+				perror("\nFailed: custom_temp_reg_write\n");
+				return 1;
+		}
+		if(temp_reg_return[0] != Temp_High_Threshold)
+		{
+					perror("\nTHigh Setting Failed\n");
+					return 1;
+		}
+		printf("\nTHigh Set at %d deg C Successfully\n", Temp_High_Threshold);
+		// Setting TLow
+		if(custom_temp_reg_write(Temp_TLow_Reg, Temp_Low_Threshold << 8))
+		{
+				perror("\nFailed: custom_temp_reg_write\n");
+				return 1;
+		}
+		if(custom_temp_reg_read(Temp_TLow_Reg, &temp_reg_return[0]))
+		{
+				perror("\nFailed: custom_temp_reg_write\n");
+				return 1;
+		}
+		if(temp_reg_return[0] != Temp_Low_Threshold)
+		{
+				perror("\nTLow Setting Failed\n");
+				return 1;
+		}
+		printf("\nTLow Set at %d deg C Successfully\n", Temp_Low_Threshold);
+		return 0;
 }
 
-int write_config_register_default( )
+uint8_t custom_test_temp_config(void)
 {
-	write_pointer_reg(0b00000001);
-	if (write(fd, &default_config_byte_one, 1) != 1)
-	{
-		perror("Configuration register write error for first byte");
-		return -1;
-	}
-	if (write(fd, &default_config_byte_two, 1) != 1)
-	{
-		perror("Configuration register write error for second byte");
-		return -1;
-	}
-	return 0;
+		static uint8_t temp_reg_return[2];
+		static uint16_t temp_config_return;
+
+		//Default
+		if(custom_temp_reg_read(Temp_Config_Reg, &temp_reg_return[0]))
+		{
+				perror("\nFailed: custom_temp_reg_write\n");
+				return 1;
+		}
+		temp_config_return = (temp_reg_return[0] << 8) | temp_reg_return[1];
+		if(temp_config_return != Temp_Config_Default)
+		{
+				perror("\nDefault Temp Config Check Failed\n");
+				printf("\nExpected: %x Got: %x\n",Temp_Config_Default , temp_config_return);
+				return 1;
+		}
+		printf("\nDefault Temp Config Check Succeeded\n");
+
+		//Shutdown Mode ON
+		if(custom_temp_reg_write(Temp_Config_Reg, Temp_Write_Shutdown_On))
+		{
+				perror("\nFailed: custom_temp_reg_write\n");
+				return 1;
+		}
+		if(custom_temp_reg_read(Temp_Config_Reg, &temp_reg_return[0]))
+		{
+				perror("\nFailed: custom_temp_reg_write\n");
+				return 1;
+		}
+		temp_config_return = (temp_reg_return[0] << 8) | temp_reg_return[1];
+		if(Temp_Test_Shutdown_On(temp_config_return))
+		{
+				perror("\nShutdown Mode ON Test Failed\n");
+				return 1;
+		}
+		printf("\nShutdown Mode ON Test Succeeded\n");
+
+		//Shutdown Mode OFF
+		if(custom_temp_reg_write(Temp_Config_Reg, Temp_Write_Shutdown_Off))
+		{
+				perror("\nFailed: custom_temp_reg_write\n");
+				return 1;
+		}
+		if(custom_temp_reg_read(Temp_Config_Reg, &temp_reg_return[0]))
+		{
+				perror("\nFailed: custom_temp_reg_write\n");
+				return 1;
+		}
+		temp_config_return = (temp_reg_return[0] << 8) | temp_reg_return[1];
+		if(Temp_Test_Shutdown_Off(temp_config_return))
+		{
+				perror("\nShutdown Mode OFF Test Failed\n");
+				return 1;
+		}
+		printf("\nShutdown Mode OFF Test Succeeded\n");
+
+		//Fault Bits - Checking both bits by setting them to 1
+		if(custom_temp_reg_write(Temp_Config_Reg, Temp_Write_Fault_Test))
+		{
+				perror("\nFailed: custom_temp_reg_write\n");
+				return 1;
+		}
+		if(custom_temp_reg_read(Temp_Config_Reg, &temp_reg_return[0]))
+		{
+				perror("\nFailed: custom_temp_reg_write\n");
+				return 1;
+		}
+		temp_config_return = (temp_reg_return[0] << 8) | temp_reg_return[1];
+		if(Temp_Test_Fault(temp_config_return))
+		{
+				perror("\nFault Bits Test Failed\n");
+				return 1;
+		}
+		printf("\nFault Bits Test Succeeded\n");
+
+		//Extended Mode Set
+		if(custom_temp_reg_write(Temp_Config_Reg, Temp_Write_Extended_Set))
+		{
+				perror("\nFailed: custom_temp_reg_write\n");
+				return 1;
+		}
+		if(custom_temp_reg_read(Temp_Config_Reg, &temp_reg_return[0]))
+		{
+				perror("\nFailed: custom_temp_reg_write\n");
+				return 1;
+		}
+		temp_config_return = (temp_reg_return[0] << 8) | temp_reg_return[1];
+		if(Temp_Test_Extended_Set(temp_config_return))
+		{
+				perror("\nExtended Mode Set Test Failed\n");
+				return 1;
+		}
+		printf("\nExtended Mode Set Test Succeeded\n");
+
+		//Extended Mode Clear
+		if(custom_temp_reg_write(Temp_Config_Reg, Temp_Write_Extended_Clear))
+		{
+				perror("\nFailed: custom_temp_reg_write\n");
+				return 1;
+		}
+		if(custom_temp_reg_read(Temp_Config_Reg, &temp_reg_return[0]))
+		{
+				perror("\nFailed: custom_temp_reg_write\n");
+				return 1;
+		}
+		temp_config_return = (temp_reg_return[0] << 8) | temp_reg_return[1];
+		if(Temp_Test_Extended_Clear(temp_config_return))
+		{
+				perror("\nExtended Mode Clear Test Failed\n");
+				return 1;
+		}
+		printf("\nExtended Mode Clear Test Succeeded\n");
+
+		//Conversion Rate
+		if(custom_temp_reg_write(Temp_Config_Reg, Temp_Write_Conversion_Test))
+		{
+				perror("\nFailed: custom_temp_reg_write\n");
+				return 1;
+		}
+		if(custom_temp_reg_read(Temp_Config_Reg, &temp_reg_return[0]))
+		{
+				perror("\nFailed: custom_temp_reg_write\n");
+				return 1;
+		}
+		temp_config_return = (temp_reg_return[0] << 8) | temp_reg_return[1];
+		if(Temp_Test_Conversion(temp_config_return))
+		{
+				perror("\nConversion Rate Test Failed\n");
+				return 1;
+		}
+		printf("\nConversion Rate Test Succeeded\n");
+
+		return 0;
 }
 
-
-
-uint16_t read_tlow_reg(int reg)
+uint8_t get_temp(float *t_data)
 {
-	uint16_t value;
-	uint8_t v[2]={0};
-	write_pointer_reg(reg);
-	if (read(fd, v, 2) != 2)
-	{
-		perror("T-low register read error");
-		return -1;
-	}
-//	value = (v[0]<<4 | (v[1] >> 4 & 0XF));
-//	value = (v[0] << 8) | (v[1] & 0xF0);
-	float val = (v[0] << 4) * (0.0625);
-	printf("T-low register value is: %x %x\n", v[0], v[1]);
-	return value;
+		static uint8_t temp_reg_return[2];
+		if(custom_temp_reg_read(Temp_Data_Reg, &temp_reg_return[0]))
+		{
+				perror("\nFailed: custom_temp_reg_write\n");
+				return 1;
+		}
+		*t_data = ((temp_reg_return[0] << 8) | temp_reg_return[1]) >> 4;
+		*t_data *= 0.0625;
+		return 0;
 }
 
-uint16_t read_temp_config_register()
+uint8_t custom_temp_init(void)
 {
-	uint16_t value;
-	uint8_t v[1]={0};
-	write_pointer_reg(0b00000001);
-	if (read(fd, v, 1) != 1)
-	{
-		perror("Temperature configuration register read error");
-		return -1;
-	}
-	value = (v[0]<<8 | v[1]);
-	printf("Temperature configuration register value is: %d \n", value);
-	return value;
+		temp_file_des = open(I2C_BUS, O_RDWR);
+		if(temp_file_des == -1)
+		{
+				perror("\nIn Temp - open() with I2C_BUS failed\n");
+				return 1;
+		}
+		if(ioctl(temp_file_des, I2C_SLAVE, Temp_Addr) == -1)
+		{
+				perror("\nIn Temp - ioctl() with I2C_BUS failed\n");
+				return 1;
+		}
+		return 0;
 }
 
-int all_temprg_rd_wr()//useless
-{
-	if (write_config_reg_on_off(1) < 0)
-	{
-		return -1;
-	}
-
-	if (write_config_reg_em(1) < 0)
-	{
-		return -1;
-	}
-
-	if (read_temp_config_register() < 0)
-	{
-		return -1;
-	}
-
-	if (write_config_register_default() < 0)
-	{
-		return -1;
-	}
-
-	if (write_config_reg_conv_rate(2) < 0)
-	{
-		return -1;
-	}
-
-	if (write_tlow_reg(0x02,45) < 0)
-	{
-		return -1;
-	}
-
-	if (read_tlow_reg(0x02) < 0)
-	{
-		return -1;
-	}
-
-	return 0;
-
-}
-
-float read_temp_data_reg(int unit)
-{
-
-	write_pointer_reg(0b00000000);
-
-	int x = read(fd,&buf,2);
-
-	if (x != 2)
-	{
-		perror("Failed to read from the i2c bus.\n");
-		printf("ERROR : %s\n", strerror(errno));
-		return -300;
-	}
-	else
-	{
-
-		MSB = buf[0];
-		LSB = buf[1];
-
-		temp = ((MSB << 8) | LSB) >> 4;
-
-		c = temp*0.0625;
-		f = (1.8 * c) + 32;
-		k = c + 273.15;
-	
-		if(unit == TEMP_CEL)
-		return c;
-		else if(unit == TEMP_KEL)
-		return k;
-		else if(unit == TEMP_FAH)
-		return f;
-	}
-}
-
-int temp_init()
-{
-	if((fd = open(bus, O_RDWR)) < 0)
-	{
-		perror("Failed to open the i2c bus");
-		return -1;
-	}
-
-	if(ioctl(fd, I2C_SLAVE, addr) < 0)
-	{
-		perror("Failed to open the i2c bus");
-		return -1;
-	}
-
-	return 0;
-}
-/*
 int main(void)
 {
-	temp_init();
-	while(1){
-	float temp = read_temp_data_reg(0);
-	printf("Current Temp: %f\n", temp);
-	}
-}*/
+		if(custom_temp_init())		perror("\nError while Initializing Temperature Sensor\n");
+		else		printf("\n>>>>>>>Temp Init Done<<<<<<<\n");
+
+		if(custom_set_temp_thresholds())	perror("\nError while setting thresholds\n");
+		else		printf("\n>>>>>>>Temp Thresholds Set<<<<<<<\n");
+
+		if(custom_test_temp_config())	perror("\nError while testing config\n");
+		else		printf("\n>>>>>>>Temp Config Test Succeeded<<<<<<<\n");
+		//Resetting Config
+		if(custom_temp_reg_write(Temp_Config_Reg, Temp_Config_Default))		perror("\nFailed: custom_temp_reg_write\n");
+
+		float t2;
+		uint8_t t_warning;
+		int i;
+		for(i = 0; i < 20; i ++)
+		{
+				if(get_temp(&t2))		perror("\nError while getting Temperature\n");
+				else			printf("\nCurrent Temp: %f", t2);
+		}
+}
--- Poorn_Files/I2C_Testing/TempSensor.h
+++ Poorn_Files/I2C_Testing/TempSensor.h
@@ -3,21 +3,60 @@
 
 #include "main.h"
 
+#define I2C_BUS		(char *)"/dev/i2c-2"
 
+#define Temp_Addr		0x48
 
-int temp_init();
+#define Temp_Data_Reg			0
+#define Temp_Config_Reg		1
+#define Temp_TLow_Reg			2
+#define Temp_THigh_Reg		3
 
-float read_temp_data_reg(int unit);
+#define Temp_Config_Default		0x6080 // 0x6080 and not 0x60A0 since I am setting THigh and TLow before reading config
 
+//Shutdown Mode
+#define Temp_Shutdown_Pos			8
+#define Temp_Shutdown_Mask		(1 << Temp_Shutdown_Pos)
+#define Temp_Write_Shutdown_On		(Temp_Config_Default | Temp_Shutdown_Mask)	// Writing 1 to the bit sets the mode ON
+#define Temp_Write_Shutdown_Off		(Temp_Config_Default & (~Temp_Shutdown_Mask))		// Writing 0 to the bit sets the mode OFF
+#define Temp_Test_Shutdown_On(x)		((x & Temp_Shutdown_Mask) >> Temp_Shutdown_Pos) == 1 ? 0 : 1
+#define Temp_Test_Shutdown_Off(x)		((x & Temp_Shutdown_Mask) >> Temp_Shutdown_Pos) == 0 ? 0 : 1
 
-int all_temprg_rd_wr();
-uint16_t read_temp_config_register();
-uint16_t read_tlow_reg(int reg);
-int write_config_register_default();
-int write_config_reg_conv_rate(uint8_t value );
-int write_config_reg_em(uint8_t value );
-int write_config_reg_on_off(uint8_t value );
-int write_tlow_reg(int reg, uint16_t value );
-void write_pointer_reg(uint8_t value);
+//Fault Bits
+#define Temp_Fault_Pos			11
+#define Temp_Fault_Mask			(3 << Temp_Fault_Pos)
+#define Temp_Write_Fault_Test		(Temp_Config_Default | Temp_Fault_Mask)	// Setting both bits 1 just for the sake of testing since this will flip the both bits from default
+#define Temp_Test_Fault(x)		((x & Temp_Fault_Mask) >> Temp_Fault_Pos) == 3 ? 0 : 1
+
+//Extended Mode
+#define Temp_Extended_Pos			4
+#define Temp_Extended_Mask		(1 << Temp_Extended_Pos)
+#define Temp_Write_Extended_Set		(Temp_Config_Default | Temp_Extended_Mask)	// Writing 1 to the bit sets the mode ON
+#define Temp_Write_Extended_Clear		(Temp_Config_Default & (~Temp_Extended_Mask))		// Writing 0 to the bit sets the mode OFF
+#define Temp_Test_Extended_Set(x)		((x & Temp_Extended_Mask) >> Temp_Extended_Pos) == 1 ? 0 : 1
+#define Temp_Test_Extended_Clear(x)		((x & Temp_Extended_Mask) >> Temp_Extended_Pos) == 0 ? 0 : 1
+
+//Conversion Rate
+#define Temp_Conversion_Pos			6
+#define Temp_Conversion_Mask			(3 << Temp_Conversion_Pos)
+#define Temp_Write_Conversion_Test		(Temp_Config_Default ^ Temp_Conversion_Mask)	// This will inverse both bits from default
+#define Temp_Test_Conversion(x)		((x & Temp_Conversion_Mask) >> Temp_Conversion_Pos) == 1 ? 0 : 1
+
+#define Temp_High_Threshold		30
+#define Temp_Low_Threshold		20
+
+#define generic_write_temp_reg(t_val, t_bytes)				(write(temp_file_des, t_val, t_bytes)) == t_bytes ? 0 : 1
+#define generic_read_temp_reg(t_val, t_bytes)				(read(temp_file_des, t_val, t_bytes)) == t_bytes ? 0 : 1
+
+#define write_reg_ptr(x)															(generic_write_temp_reg(x, 1)) == 0 ? 0 : 1
+#define write_reg(x)															(generic_write_temp_reg(x, 3)) == 0 ? 0 : 1
+#define read_reg(x)															(generic_read_temp_reg(x, 2)) == 0 ? 0 : 1
+
+uint8_t custom_temp_reg_write(uint8_t r_addr, uint16_t r_val);
+uint8_t custom_temp_reg_read(uint8_t r_addr, uint8_t *r_val);
+uint8_t custom_set_temp_thresholds(void);
+uint8_t custom_test_temp_config(void);
+uint8_t get_temp(float *t_data);
+uint8_t custom_temp_init(void);
 
 #endif
