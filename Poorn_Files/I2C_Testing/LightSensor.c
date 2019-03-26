ebcc8f71c6b55da3fa4a0292f9f06b88dd58df27
Poorn Mehta <Poorn.Mehta@Colorado.EDU>
2019-03-26 15:01:17 -0600

    Final Code of Light Sensor Interface. Includes complete BIST.

--- Poorn_Files/I2C_Testing/LightSensor.c
+++ Poorn_Files/I2C_Testing/LightSensor.c
@@ -1,420 +1,321 @@
 #include "LightSensor.h"
 
-int a;
-
-int control_reg_wr ( int fd, int msg)
-{
-int temp = 0x80;                      //for control register
-if(write(fd,&temp,1) != 1)
-{
-printf("Error in writing to control register\n");
-return -1;
-}
-
-if(write(fd,&msg,1) != 1)
-{
-printf("Error in writing message to control register\n");
-return -1;
-}
-return 0;
-}
-
-int control_reg_rd ( int fd)
-{
-int temp = 0x80 ;                     //for control register
-if(write(fd,&temp,1) != 1)
-{
-printf("Error in writing from control register\n");
-return -1;
-}
-
-if(read(fd,&temp,1) != 1)
-{
-printf("Error in reading message from control register\n");
-return -1;
-}
-return temp;
-}
-
-int timing_reg_wr ( int fd, int msg)
-{
-int temp = 0x81  ;                    //for timing register
-if(write(fd,&temp,1) != 1)
-{
-printf("Error in writing to control register\n");
-return -1;
-}
-
-if(write(fd,&msg,1) != 1)
-{
-printf("Error in writing message to control register\n");
-return -1;
-}
-return 0;
-}
-
-int timing_reg_rd(int fd){
-int temp =  0x81;                    //for timing register
-if( write(fd, &temp, 1) != 1)
-{
-printf("Error in writing to control register\n");
-return -1;
-}
-
-if( read(fd, &temp, 1) != 1)
-{
-printf("Error in writing message to control register\n");
-return -1;
-}
-
-return temp;
-}
-
-
-int control_reg_int_wr(int fd, int msg)
-{
-int temp = 0x86;                    //for interrupt control register
-if( write(fd, &temp, 1) != 1)
-{
-printf("Error in writing to interrupt control register\n");
-return -1;
-}
-
-if( write(fd, &msg, 1) != 1)
-{
-printf("Error in writing message to interrupt control register\n");
-return -1;
-}
-return 0;
-}
-
-/*read from interrupt control register */
-int control_reg_int_rd(int fd)
-{
-int temp =  0x86;
-if( write(fd, &temp, 1) != 1)
-{
-printf("Error in writing to control interrupt register\n");
-return -1;
-}
-if( read(fd, &temp, 1) != 1)
-{
-printf("Error in reading from control interrupt register\n");
-return -1;
-}
-
-return temp;
-}
-
-int threshold_int_reg_wr(int fd, int *array)
-
-{
-    int temp = 0x82;                              //for threshold low-low to threshold high-high
-    if( write(fd, &temp, 1) != 1)
-         {
-        printf("Unable to write to threshhold interrupt register\n");
-        return -1;
-    }
-    temp = array[0];
-    if( write(fd, &temp, 1) != 1)
-         {
-        printf("Unable to write to threshhold interrupt register\n");
-        return -1;
-    }
-
-    temp = 0x83;
-    if( write(fd, &temp, 1) != 1)
-    {
-        printf("Unable to write to threshhold interrupt register\n");
-        return -1;
-    }
-    temp = array[1];
-    if( write(fd, &temp, 1) != 1)
-    {
-    printf("Unable to write to threshhold interrupt register\n");
-        return -1;
-    }
-
-    temp = 0x84;
-    if( write(fd, &temp, 1) != 1)
-    {
-        printf("Unable write to threshhold interrupt register\n");
-        return -1;
-    }
-    temp = array[2];
-
-    if( write(fd, &temp, 1) != 1)
-    {
-        printf("Unable to write to threshhold interrupt register\n");
-        return -1;
-    }
-
-    temp = 0x85;
-    if( write(fd, &temp, 1) != 1)
-    {
-        printf("Unable to write to threshhold interrupt register\n");
-        return -1;
-    }
-    temp = array[3];
-    if( write(fd, &temp, 1) != 1)
-    {
-        printf("Unable to write to threshhold interrupt register\n");
-        return -1;
-    }
-    return 0;
-
-}
-
-int threshold_int_reg_rd(int fd, int *array){
-
-    int temp =  0x82 ;                          //for threshold low-low to threshold high-high
-    if( write(fd, &temp, 1) != 1){
-        printf("Unable to write to threshhold interrupt register\n");
-        return -1;
-    }
-    if( read(fd, &temp, 1) != 1){
-        printf("Unable to read from threshhold interrupt register\n");
-        return -1;
-    }
-    array[0] = temp;
-
-    temp =  0x83;
-    if( write(fd, &temp, 1) != 1){
-        printf("Unable to write to threshhold interrupt register\n");
-        return -1;
-    }
-    if( read(fd, &temp, 1) != 1){
-        printf("Unable to read from threshhold interrupt register\n");
-        return -1;
-    }
-    array[1] = temp;
-
-    temp =  0x84 ;
-    if( write(fd, &temp, 1) != 1){
-        printf("Unable to write to threshhold interrupt register\n");
-        return -1;
-    }
-    if( read(fd, &temp, 1) != 1){
-        printf("Unable to read from threshhold interrupt register\n");
-        return -1;
-    }
-    array[2] = temp;
-
-    temp =  0x85 ;
-    if( write(fd, &temp, 1) != 1){
-        printf("Unable to write to threshhold interrupt register\n");
-        return -1;
-    }
-    if( read(fd, &temp, 1) != 1){
-        printf("Unable to read from threshhold interrupt register\n");
-        return -1;
-    }
-    array[3] = temp;
-    return 0;
-
-}
-
-int id_reg_rd(int fd)
-{
-    int temp =  0x8A;                             //for id register
-    if( write(fd, &temp, 1) != 1){
-        printf("Unable to write to id register\n");
-        return -1;
-    }
-    if( read(fd, &temp, 1) != 1){
-        printf("Unable to read from id register\n");
-        return -1;
-    }
-
-    return 0;
-}
-
-uint16_t data0_reg_rd(int fd){
-    int temp =  0x8C;
-
-    if( write(fd, &temp, 1) != 1)
-    {
-        printf("Unable to write to data 0 register\n");
-        return -1;
-    }
-    uint8_t dlow0;
-
-    if( read(fd, &dlow0, 1) != 1)
-    {
-        printf("Unable to read from data 0 register\n");
-        return -1;
-    }
-
-    temp =  0x8D;
-    if( write(fd, &temp, 1) != 1)
-    {
-        printf("Unable to write to data 0 register\n");
-        return -1;
-    }
-    uint16_t dhigh0;
-
-    if( read(fd, &dhigh0, 1) != 1)
-    {
-        printf("Unable to read from write 0 register\n");
-        return -1;
-    }
-
-
-    uint16_t final = dhigh0<<8 | dlow0;
-    return final;
-}
-
-uint16_t data1_reg_rd(int fd){
-    int temp =  0x8E;
-
-    if( write(fd, &temp, 1) != 1)
-    {
-        printf("Unable to write to data 1 register\n");
-        return -1;
-    }
-    uint8_t dlow1;
-
-    if( read(fd, &dlow1, 1) != 1)
-    {
-        printf("Unable to read from data 1 register\n");
-        return -1;
-    }
-
-    temp =  0x8F;
-    if( write(fd, &temp, 1) != 1)
-    {
-        printf("Unable to write to data 1 register\n");
-        return -1;
-    }
-    uint16_t dhigh1;
-
-    if( read(fd, &dhigh1, 1) != 1)
-    {
-        printf("Unable to read from write 1 register\n");
-        return -1;
-    }
-
-
-    uint16_t final = dhigh1<<8 | dlow1;
-    return final;
-}
-
-int all_reg_rd_wr(int fd)
-
-        {
-    int out;
-    out = control_reg_wr(fd, 0x03);
-    if( out ==  -1 )
-        return -1;
-
-    out = control_reg_rd(fd);
-    if( out ==  -1 )
-        return -1;
-    out = timing_reg_wr(fd, 0x12);
-    if( out ==  -1 )
-        return -1;
-    out = timing_reg_rd(fd);
-    if( out ==  -1 )
-        return -1;
-
-    int array[4] = {0, 0, 0, 0};
-
-    out = threshold_int_reg_rd(fd, array);
-    if( out ==  -1 )
-    return -1;
-
-        int arr[1] = {0x0F};
-    out = threshold_int_reg_wr(fd, arr);
-    if( out ==  -1 )
-        return -1;
-
-    out = id_reg_rd(fd);
-    if( out ==  -1 )
-        return -1;
-
-    out = data0_reg_rd(fd);
-    if( out ==  -1 )
-        return -1;
-
-    out = data1_reg_rd(fd);
-    if( out ==  -1 )
-        return -1;
-
-    return 0;
-
-}
-
-int light_init(void)
-{
-    int file;
-
-    char *myfile = "/dev/i2c-2";
-    file = open(myfile, O_RDWR);
-    if (file < 0)
-     {
-        perror("Unable to open the i2c file.\n");
-        return -1;
-    }
-    int addr = 0x39; //The I2C slave address
-
-    if (ioctl(file, I2C_SLAVE, addr) < 0)
-     {
-        perror("Unable to use ioctl call.\n");
-        return -1;
-    }
-    a=file;
-    return 0;
-}
-
-float get_lux()
-{
-    float ch_0 = 0, ch_1 = 0;
-    float adc,lux;
-
-    if(control_reg_wr(a, 0x03) < 0) //to power up the sensor
-    {
-          return -1;
-        }
-        if(timing_reg_wr(a, time_high|gain) < 0)
-        {
-     return -1;
-    }
-
-
-    ch_0 = (float)data0_reg_rd(a);
-    ch_1 = (float)data1_reg_rd(a);
-
-    adc = ch_1/ch_0;
-
-    if(adc>0 && adc <= 0.5)
-    return lux = (0.0304 * ch_0) - (0.062 * ch_0 * pow(adc, 1.4));
-
-    else if((adc>0.5) && (adc<=0.61))
-    return lux = (0.0224 * ch_0) - (0.031 * ch_1);
-
-        else if((adc>0.61)&&(adc<=0.80))
-        return lux = (0.0128 * ch_0) - (0.0153 * ch_1);
-
-        else if((adc>0.80) && (adc<=1.30))
-        return lux = (0.00146 * ch_0) - (0.00112 * ch_1);
-
-        else if(adc > 1.30)
-        return lux = 0;
-
-return -1;
-}
 /*
+*		File: LightSensor.c
+*		Purpose: To interface with Light Sensor (APDS9301) on I2C bus, on BeagleBoneGreen, running custom linux kernel (platform by Buildroot)
+*		Owner: Poorn Mehta
+*		Date: 3/26/2019
+*/
+
+//Initializing global variable - this will contain I2C bus's file descriptor
+int lux_file_des = -1;
+
+/*
+*		Function to write to any internal register of Light Sensor
+*		Returns 1 on failure, 0 on success
+*		First Parameter is Internal Address of the Register
+*		Second Parameter is the value to be written
+*/
+uint8_t custom_lux_reg_write(uint8_t r_addr, uint8_t r_val)
+{
+		static uint8_t lux_data_write = 0;
+		lux_data_write = Lux_Command_Reg_Data(r_addr);
+
+		if((r_addr == Lux_Res_Reg1) || (r_addr == Lux_Res_Reg2) || (r_addr == Lux_Res_Reg3) || (r_addr == Lux_Res_Reg4))		// Checking whether the passed register address is valid and writeable or not
+		{
+				printf("\nInvalid Register Address Supplied (Lux Sensor)\n");
+				return 1;
+		}
+
+		if(write_reg_cmd(&lux_data_write))		// Writing to command register first
+		{
+				printf("\nError in write_reg_cmd\n");
+				return 1;
+		}
+		if(lux_write_reg(&r_val, 1))		// Writing to the actual register
+		{
+				printf("\nError in lux_write_reg\n");
+				return 1;
+		}
+		return 0;
+}
+
+/*
+*		Function to read from any internal register of Light Sensor
+*		Returns 1 on failure, 0 on success
+*		First Parameter is Internal Address of the Register
+*		Second Parameter is the pointer to destination variable
+*/
+uint8_t custom_lux_reg_read(uint8_t r_addr, uint8_t *r_val)
+{
+		static uint8_t lux_data_write = 0;
+		lux_data_write = Lux_Command_Reg_Data(r_addr);
+
+		if(write_reg_cmd(&lux_data_write))		// Writing to command register
+		{
+				printf("\nError in write_reg_cmd\n");
+				return 1;
+		}
+		if(lux_read_reg(r_val, 1))		// Reading the data, and storing in the pointer through macro
+		{
+				printf("\nError in lux_read_reg\n");
+				return 1;
+		}
+		return 0;
+}
+
+/*
+*		Function to test all settings of Temperature Sensor through manipulation of various registers
+*		Returns 1 on failure, 0 on success
+*		No Parameters
+*/
+uint8_t custom_test_lux_config(void)
+{
+		static uint8_t lux_reg_return;
+
+		// Powering ON the Sensor by writing 0x03 to Control Register
+		if(custom_lux_reg_write(Lux_Control_Reg, Lux_Control_Power_ON))
+		{
+				printf("\nError in custom_lux_reg_write\n");
+				return 1;
+		}
+		// Reading back Control Register
+		if(custom_lux_reg_read(Lux_Control_Reg, &lux_reg_return))
+		{
+				printf("\nError in custom_lux_reg_read\n");
+				return 1;
+		}
+		// Verifying value 0x03
+		if(Lux_Test_Control_Power(lux_reg_return))
+		{
+				printf("\nLux Power ON Test Failed %x\n", lux_reg_return);
+				return 1;
+		}
+		printf("\nLux Power ON Test Completed Successfully\n");
+
+		// Setting High Gain and High Integration Time, and Verifying the same
+		if(custom_lux_reg_write(Lux_Timing_Reg, Lux_Set_Gain_High(Lux_High_Integration_Time)))
+		{
+				printf("\nError in custom_lux_reg_write\n");
+				return 1;
+		}
+		// Reading back Timing Register
+		if(custom_lux_reg_read(Lux_Timing_Reg, &lux_reg_return))
+		{
+				printf("\nError in custom_lux_reg_read\n");
+				return 1;
+		}
+		// Verifying values
+		if((Lux_Test_High_Int_Time(lux_reg_return)) || (Lux_Test_Gain_High(lux_reg_return)))
+		{
+				printf("\nLux Gain and Integration Time Test Failed %x\n", lux_reg_return);
+				return 1;
+		}
+		printf("\nLux Gain and Integration Time Test Completed Successfully\n");
+
+		//Testing Interrupt Control Register with Test Data 0x0F
+		if(custom_lux_reg_write(Lux_Intrp_Ctrl_Reg, Lux_Interrupt_Test_Data))
+		{
+				printf("\nError in custom_lux_reg_write\n");
+				return 1;
+		}
+		// Reading back Interrupt Control Register
+		if(custom_lux_reg_read(Lux_Intrp_Ctrl_Reg, &lux_reg_return))
+		{
+				printf("\nError in custom_lux_reg_read\n");
+				return 1;
+		}
+		// Verifying values
+		if(Lux_Test_Intrp_Ctrl_Data(lux_reg_return))
+		{
+				printf("\nLux Interrupt Control Register Test Failed %x\n", lux_reg_return);
+				return 1;
+		}
+		printf("\nLux Interrupt Control Register Test Completed Successfully\n");
+		// Reverting back
+		if(custom_lux_reg_write(Lux_Intrp_Ctrl_Reg, 0))
+		{
+				printf("\nError in custom_lux_reg_write\n");
+				return 1;
+		}
+
+		// Interrupt Thresholds - Low and High Test with predefined random data
+		// Using Word Mode
+		static uint8_t lux_data_write[2] = {0};
+
+		// Writing TLow
+		lux_data_write[0] = Lux_Command_Word_Data(Lux_ThrLow_Low_Reg);
+		if(write_reg_cmd(&lux_data_write[0]))		// Writing to command register first
+		{
+				printf("\nError in write_reg_cmd\n");
+				return 1;
+		}
+		lux_data_write[0] = Lux_ThrLow_Low_Test_Data;
+		lux_data_write[1] = Lux_ThrLow_High_Test_Data;
+		if(lux_write_reg(&lux_data_write[0], 2))
+		{
+				printf("\nError in lux_write_reg\n");
+				return 1;
+		}
+		// Reading TLow
+		lux_data_write[0] = Lux_Command_Word_Data(Lux_ThrLow_Low_Reg);
+		if(write_reg_cmd(&lux_data_write[0]))
+		{
+				printf("\nError in write_reg_cmd\n");
+				return 1;
+		}
+		if(lux_read_reg(&lux_data_write[0], 2))
+		{
+				printf("\nError in lux_read_reg\n");
+				return 1;
+		}
+		// Verifying TLow
+		if((Lux_Test_ThrLow_Low(lux_data_write[0]) == Lux_ThrLow_Low_Test_Data) || (Lux_Test_ThrLow_High(lux_data_write[1]) == Lux_ThrLow_High_Test_Data))
+		{
+				printf("\nLux Interrupt Threshold TLow Test failed\n");
+				return 1;
+		}
+		printf("\nLux Interrupt Threshold TLow Test Completed Successfully\n");
+
+		// Writing THigh
+		lux_data_write[0] = Lux_Command_Word_Data(Lux_ThrHigh_Low_Reg);
+		if(write_reg_cmd(&lux_data_write[0]))
+		{
+				printf("\nError in write_reg_cmd\n");
+				return 1;
+		}
+		lux_data_write[0] = Lux_ThrHigh_Low_Test_Data;
+		lux_data_write[1] = Lux_ThrHigh_High_Test_Data;
+		if(lux_write_reg(&lux_data_write[0], 2))
+		{
+				printf("\nError in lux_write_reg\n");
+				return 1;
+		}
+		// Reading THigh
+		lux_data_write[0] = Lux_Command_Word_Data(Lux_ThrHigh_Low_Reg);
+		if(write_reg_cmd(&lux_data_write[0]))
+		{
+				printf("\nError in write_reg_cmd\n");
+				return 1;
+		}
+		if(lux_read_reg(&lux_data_write[0], 2))
+		{
+				printf("\nError in lux_read_reg\n");
+				return 1;
+		}
+		// Verifying THigh
+		if((Lux_Test_ThrHigh_Low(lux_data_write[0]) == Lux_ThrHigh_Low_Test_Data) || (Lux_Test_ThrHigh_High(lux_data_write[1]) == Lux_ThrHigh_High_Test_Data))
+		{
+				printf("\nLux Interrupt Threshold THigh Test failed\n");
+				return 1;
+		}
+		printf("\nLux Interrupt Threshold THigh Test Completed Successfully\n");
+
+		// Reading ID Register for testing part and revision number
+		if(custom_lux_reg_read(Lux_ID_Reg, &lux_reg_return))
+		{
+				printf("\nError in custom_lux_reg_read\n");
+				return 1;
+		}
+		// Verifying values (part number)
+		if(Lux_Test_Part_No(lux_reg_return))
+		{
+				printf("\nLux ID Register Register Test Failed %x\n", lux_reg_return);
+				return 1;
+		}
+		printf("\nLux ID Register Test Succeeded\n");
+
+		return 0;
+}
+
+/*
+*		Function to initialize light sensor
+*		Returns 1 on failure, 0 on success
+*		No Parameters
+*/
+uint8_t custom_lux_init(void)
+{
+		lux_file_des = open(I2C_BUS, O_RDWR);
+		if(lux_file_des == -1)
+		{
+				printf("\nIn Lux - open() with I2C_BUS failed\n");
+				return 1;
+		}
+		if(ioctl(lux_file_des, I2C_SLAVE, Lux_Addr) == -1)
+		{
+				printf("\nIn Lux - ioctl() with I2C_BUS failed\n");
+				return 1;
+		}
+		return 0;
+}
+
+/*
+*		Function to read light level in lumens
+*		Returns 1 on failure, 0 on success
+*		First Parameter is the pointer to destination variable(float)
+*/
+uint8_t get_lux(float *l_data)
+{
+		static float lux_ch0 = 0;
+		static float lux_ch1 = 0;
+		static float ratio = 0;
+
+		// Using Word Mode
+		static uint8_t lux_data_write[2] = {0};
+
+		// Reading Ch0
+		lux_data_write[0] = Lux_Command_Word_Data(Lux_Data0_Low);
+		if(write_reg_cmd(&lux_data_write[0]))
+		{
+				printf("\nError in write_reg_cmd\n");
+				return 1;
+		}
+		if(lux_read_reg(&lux_data_write[0], 2))
+		{
+				printf("\nError in lux_read_reg\n");
+				return 1;
+		}
+		lux_ch0 = (float)((lux_data_write[1] << 8) | lux_data_write[0]);
+
+		// Reading Ch1
+		lux_data_write[0] = Lux_Command_Word_Data(Lux_Data1_Low);
+		if(write_reg_cmd(&lux_data_write[0]))
+		{
+				printf("\nError in write_reg_cmd\n");
+				return 1;
+		}
+		if(lux_read_reg(&lux_data_write[0], 2))
+		{
+				printf("\nError in lux_read_reg\n");
+				return 1;
+		}
+		lux_ch1 = (float)((lux_data_write[1] << 8) | lux_data_write[0]);
+
+		ratio = lux_ch1 / lux_ch0;
+
+		// Calculation is Based on Datasheet
+		if((ratio > 0) && (ratio <= 0.5))		*l_data = (0.0304 * lux_ch0) - (0.062 * lux_ch0 * pow(ratio, 1.4));
+		else if((ratio > 0.5) && (ratio <= 0.61))		*l_data = (0.0224 * lux_ch0) - (0.031 * lux_ch1);
+		else if((ratio > 0.61) && (ratio <= 0.80))		*l_data = (0.0128 * lux_ch0) - (0.0153 * lux_ch1);
+		else if((ratio > 0.80) && (ratio <= 1.30))		*l_data = (0.00146 * lux_ch0) - (0.00112 * lux_ch1);
+		else		*l_data = 0;
+
+		return 0;
+}
+
 int main()
-	{int file;
-	file = light_init();
-	time_t curtime;
-	time(&curtime);
-	while(1)
-	{
-	float lumen = get_lux();
-	printf("Time stamp: %s",ctime(&curtime));
-	printf("Length of time stamp : %ld",strlen(ctime(&curtime)));
-	printf("The current lux is %f\n", lumen);  
-	}
-	return 0;
-	}*/
+{
+		if(custom_lux_init())		printf("\nError while Initializing Lux Sensor\n");
+		else		printf("\n>>>>>>>Lux Init Done<<<<<<<\n");
+
+		if(custom_test_lux_config())	printf("\nError while testing config\n");
+		else		printf("\n>>>>>>>Lux Config Test Succeeded<<<<<<<\n");
+
+		float lux;
+		int i;
+		for(i = 0; i < 10; i ++)
+		{
+			if(get_lux(&lux))		printf("\nError while getting lux\n");
+			else		printf("\nCurrent Lux Level is: %f\n", lux);
+		}
+}
--- Poorn_Files/I2C_Testing/LightSensor.h
+++ Poorn_Files/I2C_Testing/LightSensor.h
@@ -3,25 +3,110 @@
 
 #include "main.h"
 
-#define time_high 0x02  //for 402ms
-#define time_med 0x01  //for 101ms
-#define time_low 0x00  //for 13ms
+/*
+*		File: LightSensor.h
+*		Purpose: To interface with Light Sensor (APDS9301) on I2C bus, on BeagleBoneGreen, running custom linux kernel (platform by Buildroot)
+*		Owner: Poorn Mehta
+*		Date: 3/26/2019
+*/
 
-#define gain 0x10  //for maximum gain
+// Address of light sensor
+#define Lux_Addr	0x39
 
-int control_reg_wr ( int fd, int msg);
-int control_reg_rd ( int fd);
-int timing_reg_wr ( int fd, int msg);
-int timing_reg_rd(int fd);
-int control_reg_int_wr(int fd, int msg);
-int control_reg_int_rd(int fd);
-int threshold_int_reg_wr(int fd, int *array);
-int threshold_int_reg_rd(int fd, int *array);
-int id_reg_rd(int fd);
-uint16_t data0_reg_rd(int fd);
-uint16_t data1_reg_rd(int fd);
-float get_lux(void);
-int light_init(void);
-int all_reg_rd_wr(int fd);
+// Internal register addresses
+#define Lux_Control_Reg					0x00
+#define Lux_Timing_Reg					0x01
+#define Lux_ThrLow_Low_Reg			0x02
+#define Lux_ThrLow_High_Reg			0x03
+#define Lux_ThrHigh_Low_Reg			0x04
+#define Lux_ThrHigh_High_Reg		0x05
+#define Lux_Intrp_Ctrl_Reg			0x06
+#define Lux_ID_Reg							0x0A
+#define Lux_Data0_Low						0x0C
+#define Lux_Data0_High					0x0D
+#define Lux_Data1_Low						0x0E
+#define Lux_Data1_High					0x0F
+
+// Reserved internal register addresses
+#define Lux_Res_Reg1						0x07
+#define Lux_Res_Reg2						0x08
+#define Lux_Res_Reg3						0x09
+#define Lux_Res_Reg4						0x0B
+
+// ALL Macros returs 0 on success and 1 on failure
+
+// Defines and macros for gain and integration time settings
+#define Lux_Integration_Pos			0
+#define Lux_Integration_Mask		(3 << Lux_Integration_Pos)
+#define Lux_Low_Integration_Time		(0 << Lux_Integration_Pos)		// 13.7ms
+#define Lux_Test_Low_Int_Time(x)		((x & Lux_Integration_Mask) >> Lux_Integration_Pos) == Lux_Low_Integration_Time ? 0 : 1
+#define Lux_Med_Integration_Time		(1 << Lux_Integration_Pos)		// 101ms
+#define Lux_Test_Med_Int_Time(x)		((x & Lux_Integration_Mask) >> Lux_Integration_Pos) == Lux_Med_Integration_Time ? 0 : 1
+#define Lux_High_Integration_Time		(2 << Lux_Integration_Pos)		// 402ms
+#define Lux_Test_High_Int_Time(x)		((x & Lux_Integration_Mask) >> Lux_Integration_Pos) == Lux_High_Integration_Time ? 0 : 1
+
+#define Lux_Gain_Pos					4
+#define Lux_Gain_Mask					(1 << Lux_Gain_Pos)
+#define Lux_Set_Gain_Low(x)				x &	~(Lux_Gain_Mask)
+#define Lux_Test_Gain_Low(x)			((x &	Lux_Gain_Mask) >> Lux_Gain_Pos) == 0 ? 0 : 1
+#define Lux_Set_Gain_High(x)				x |	Lux_Gain_Mask
+#define Lux_Test_Gain_High(x)			((x &	Lux_Gain_Mask) >> Lux_Gain_Pos) == 1 ? 0 : 1
+
+// Defines and macros for interrupt controller manipulation
+#define Lux_Interrupt_Test_Data			0x0F
+#define	Lux_Interrupt_Control_Mask		0x0F
+#define Lux_Test_Intrp_Ctrl_Data(x)		(x & Lux_Interrupt_Control_Mask) == Lux_Interrupt_Test_Data ? 0 : 1
+
+// Defines and macros for interrupt threshold registers manipulation
+#define Lux_ThrLow_Low_Test_Data			0xA5
+#define Lux_ThrLow_High_Test_Data			0x5A
+#define Lux_ThrHigh_Low_Test_Data			0x0F
+#define Lux_ThrHigh_High_Test_Data		0xF0
+#define Lux_Test_ThrLow_Low(x)				x == Lux_ThrLow_Low_Test_Data ? 0 : 1
+#define Lux_Test_ThrLow_High(x)				x == Lux_ThrLow_High_Test_Data ? 0 : 1
+#define Lux_Test_ThrHigh_Low(x)				x == Lux_ThrHigh_Low_Test_Data ? 0 : 1
+#define Lux_Test_ThrHigh_High(x)				x == Lux_ThrHigh_High_Test_Data ? 0 : 1
+
+// Defines and macros for verifying ID register of the sensor
+#define Lux_Part_No				5				// Fixed value, from datasheet
+#define Lux_Part_No_Pos		4
+#define Lux_Part_No_Mask	(Lux_Part_No << Lux_Part_No_Pos)
+#define Lux_Test_Part_No(x)		((x & Lux_Part_No_Mask) >> Lux_Part_No_Pos) == Lux_Part_No ? 0 : 1
+
+//Macro that uses write() API for writing to I2C bus, returns 0 on success, 1 on failure
+#define generic_write_lux_reg(t_val, t_bytes)				(write(lux_file_des, t_val, t_bytes)) == t_bytes ? 0 : 1
+
+//Macro that uses read() API for reading from I2C bus, returns 0 on success, 1 on failure
+#define generic_read_lux_reg(t_val, t_bytes)				(read(lux_file_des, t_val, t_bytes)) == t_bytes ? 0 : 1
+
+// Defines and macros for using command register with high flexibility
+#define Lux_Command_Bit_Pos			7
+#define Lux_Command_Reg_Mask		(1 << Lux_Command_Bit_Pos)
+#define Lux_Command_Address_Mask		(0x0F)
+#define Lux_Command_Reg_Data(x)		((x & Lux_Command_Address_Mask) | Lux_Command_Reg_Mask)
+#define Lux_Word_Mode_Bit_Pos		5
+#define Lux_Word_Mode_Mask		Lux_Command_Reg_Mask | (1 << Lux_Word_Mode_Bit_Pos)
+#define Lux_Command_Word_Data(x)		((x & Lux_Command_Address_Mask) | Lux_Word_Mode_Mask)
+//Macro to write to command register
+#define write_reg_cmd(x)															(generic_write_lux_reg(x, 1)) == 0 ? 0 : 1
+
+//Macro to write to any other internal register, after writing to command register
+#define lux_write_reg(x, t_bytes)															(generic_write_lux_reg(x, t_bytes)) == 0 ? 0 : 1
+
+//Macro to read from any internal register, after writing to command register
+#define lux_read_reg(x, t_bytes)															(generic_read_lux_reg(x, t_bytes)) == 0 ? 0 : 1
+
+// Macros and defines for turning ON the power to the sensor
+#define Lux_Control_Power_ON		0x03
+#define Lux_Control_Pos					0
+#define Lux_Control_Mask				((Lux_Control_Power_ON) << Lux_Control_Pos)
+#define Lux_Test_Control_Power(x)		(x & Lux_Control_Mask) == Lux_Control_Power_ON ? 0 : 1
+
+// Function prototypes
+uint8_t custom_lux_reg_write(uint8_t r_addr, uint8_t r_val);
+uint8_t custom_lux_reg_read(uint8_t r_addr, uint8_t *r_val);
+uint8_t custom_test_lux_config(void);
+uint8_t custom_lux_init(void);
+uint8_t get_lux(float *l_data);
 
 #endif
